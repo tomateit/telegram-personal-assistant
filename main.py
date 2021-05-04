a4ec46b0bcec0a6a54d3797cb57876813f05617a
Max Ermakov <maksim_ermakov@protonmail.com>
2021-05-04 12:02:59 +0000

    Adding functionality

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 from telethon import TelegramClient, events, sync
 import os
+import logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
 load_dotenv()
 
 API_ID = os.environ["API_ID"]
@@ -9,18 +12,71 @@ API_HASH = os.environ["API_HASH"]
 api_id = API_ID
 api_hash = API_HASH
 
-client = TelegramClient('session_name', api_id, api_hash)
+client = TelegramClient('TelegramPA_session', api_id, api_hash)
 client.start()
+me = client.get_me()
 
-print(client.get_me().stringify())
+async def main():
+    async for draft in client.iter_drafts():
+        print(draft.text)
+    # print(me.stringify())
+    # username = me.username
+    # print(username)
+    # print(me.phone)
+    # client.send_message('me', 'Hello! Talking to you from Telethon')
 
-client.send_message('trent', 'Hello! Talking to you from Telethon')
-# client.send_file('username', '/home/myself/Pictures/holidays.jpg')
+    # You can print all the dialogs/conversations that you are part of:
+    # async for dialog in client.iter_dialogs():
+    #     print(dialog.name, 'has ID', dialog.id)
 
-# client.download_profile_photo('me')
-# messages = client.get_messages('username')
-# messages[0].download_media()
+    # You can send messages to yourself...
+    # message = await client.send_message('me', 'Hello, myself!')
 
-@client.on(events.NewMessage(pattern='(?i)hi|hello'))
-async def handler(event):
-    await event.respond('Hey!')
+    # Sending a message returns the sent message object, which you can use
+    # print(message.raw_text)
+
+    # You can reply to messages directly if you have a message object
+    # await message.reply('Cool!')
+
+# @client.on(events.NewMessage(pattern='(?i)hi|hello'))
+# async def handler(event):
+#     await event.respond('Hey!')
+
+UPPERCASE_PATTERN = r'^\$~$'
+import re
+# @client.on(events.NewMessage(outgoing=True, pattern=UPPERCASE_PATTERN))
+# async def uppercase_command(event):
+#     chat = await event.get_chat()
+#     # chat_id = event.chat_id
+#     sender = await event.get_sender()
+#     # sender_id = event.sender_id
+#     if sender != client.get_me():
+#         raise RuntimeError("Only I can trigger this")
+
+#     n_iter, limit_iter = 0, 4
+#     async for message in client.iter_messages(chat):
+#         n_iter += 1
+#         if event.id != message.id:
+#             if (await message.get_sender() == me):
+#                 text = message.raw_text
+#                 await message.edit(text)
+#                 print("Edited message")
+#                 break
+#         if n_iter == limit_iter:
+#             print("Too many iterations")
+#             break
+
+EDITED_UPPERCASE_PATTERN = r'\$~'
+@client.on(events.MessageEdited(outgoing=True, pattern=EDITED_UPPERCASE_PATTERN))
+async def uppercase_command(event):
+    text = event.message.raw_text
+    # chat_id = event.chat_id
+    await event.message.edit(text.upper())
+
+if __name__ == "__main__":
+    with client:
+        try:
+            client.loop.run_until_complete(main())
+        except Exception as e:
+            logging.exception(e) 
+            client.log_out()
